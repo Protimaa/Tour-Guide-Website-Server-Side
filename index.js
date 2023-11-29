8a5fad76f4915981624b0e057cdb4da8ac0a6f73
Protima <36505000+Protimaa@users.noreply.github.com>
2023-11-29 10:46:45 +0600

    admin created successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,12 +34,51 @@ async function run() {
         const reviewsCollection = client.db("dp-TouristGuideDB").collection("reviews");
         const bookedServiceCollection = client.db("dp-TouristGuideDB").collection("bookedService");
 
+        // for jwt
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
+        // middlewares 
+        const verifyToken = (req, res, next) => {
+
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
 
         // for user 
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
+            // console.log(req.header)
             const result = await userCollection.find().toArray();
             res.send(result);
         });
+        // for admin 
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
 
 
         app.post('/users', async (req, res) => {
@@ -56,15 +96,15 @@ async function run() {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
-              $set: {
-                role: 'admin'
-              }
+                $set: {
+                    role: 'admin'
+                }
             }
             const result = await userCollection.updateOne(filter, updatedDoc);
             res.send(result);
-          })
+        })
 
-        app.delete('/users/:id',   async (req, res) => {
+        app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query);
